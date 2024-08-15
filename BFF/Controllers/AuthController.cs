c2dd6af2c895608d740012aaf4c0fecd59199576
randelramirez <randelramirez1@gmail.com>
2024-08-15 20:20:02 +0800

    Add comments

--- BFF/Controllers/AuthController.cs
+++ BFF/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Auth0.AspNetCore.Authentication;
 
 namespace BackendForFrontend.Controllers;
 
@@ -17,6 +18,19 @@ public class AuthController : Controller
         var result = new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
         return result;
     }
+    
+    // same (what happens under the hood...)
+    // public async Task Login(string returnUrl = "/")
+    // {
+    //     var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+    //         // Indicate here where Auth0 should redirect the user after a login.
+    //         // Note that the resulting absolute Uri must be added to the
+    //         // **Allowed Callback URLs** settings for the app.
+    //         .WithRedirectUri(returnUrl)
+    //         .Build();
+    //
+    //     await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    // }
 
     [Authorize]
     public async Task<ActionResult> Logout()
